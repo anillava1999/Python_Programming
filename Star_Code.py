8bb21ac71b5ff268f66378a0488ebaa7540244e4
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2024-05-29 13:50:21 +0530

    Create Star_Code.py

---
+++ Star_Code.py
@@ -0,0 +1,15 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+
+for i in range(1,6): # consider it as row 
+    for j in range(i): # consider it as column
+        print("*", end =' ')
+    print()
+
+        
+            
