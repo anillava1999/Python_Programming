29d361bff329beb2635f4d3f0ce067b32bd4f6ed
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2024-05-29 14:27:07 +0530

    Create Start_Code_Second.py

---
+++ Start_Code_Second.py
@@ -0,0 +1,13 @@
+'''
+      *
+     ***
+    *****
+   *******
+  *********
+ ***********
+'''
+
+for i in range(1,6+1): # consider it as row 
+    print(' ' * (6-i), end= '') #it's for first spaces
+    print('*' * (2*i-1)) #it's for creating star
+            
